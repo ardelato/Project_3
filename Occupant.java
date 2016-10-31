8509148d5d5e748e4b84780d030e297596e9a171
ardelato <angel.dlt.1996@gmail.com>
2016-10-31 13:28:58 -0700

    Code Added

--- Occupant.java
+++ Occupant.java
@@ -9,26 +9,39 @@
  *@version Fall 2016
  *@version 10/28/16
  */
-
+public abstract class Occupant
+{
 //instance variables
 	//make a Square type variable representing location of the object 
 	/* declare its privacy then the data type Square followed by the name 
 	you wish to represent for location*/
+	private Square square_location;
 
 //constructors
 	//make an empty constructor called Occupant
+	public Occupant();
 	//make a constructor called Occupant what will accept a parameter of Square type.
 		/*this constructor will make our instance variable above equal to the value
 		 of the value passed parameter variable
 		 */
+	public Occupant(Square start)
+	{
+		square_location = start; 
+	}
+
 //Methods
 	//make a function that will return the location 
 	//of the object based on the square they're in
-		 /*Simply return the value of Square variable you have ealier in the class.
+		 /*Simply return the value of Square variable you have ealier in the class.*/
+	public Square location()
+	{ return square_location;}
 	//make a function that will move an object's location based of the 
 	//the location you want
 		/*Simply set you Square variable equal to the new Square and then return it*/
-
-
+	public void moveTo(Square new_location)
+	{
+		square_location = new_location;
+	}
+}
 
 	
